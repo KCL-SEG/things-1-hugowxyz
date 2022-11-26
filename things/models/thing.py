77e5e34abfb1137f373e71f02f67e7c95f2ebe0b
hugowxyz1 <39029526+hugowxyz@users.noreply.github.com>
2022-11-26 09:51:50 +0000

    Create thing.py

---
+++ things/models/thing.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Thing(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=500)
+    quantity = models.IntegerField()
